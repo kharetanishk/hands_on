4b09171bf428837dadbc3837eeaaf941a767cebe
Tanishk khare <kharetanishk18@gmail.com>
2026-05-17 11:55:53 +0530

    seeded the admin

--- apps/backend/src/models/UserModel.ts
+++ apps/backend/src/models/UserModel.ts
@@ -1,15 +1,23 @@
 import { Schema, model } from "mongoose";
 
+type Roles = "student" | "teacher" | "admin";
+
 interface User {
   username: string;
   email: string;
   password: string;
+  role: Roles;
 }
 
 const UserSchema = new Schema<User>({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: {
+    type: String,
+    enums: ["student", "teacher", "admin"],
+    default: "student",
+  },
 });
 
 export const UserModel = model<User>("user", UserSchema);
