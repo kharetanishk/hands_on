5f57ff30bdcf2b60d1ea024cf219ccb33e3bed12
Tanishk khare <kharetanishk18@gmail.com>
2026-05-19 20:35:34 +0530

    re designed the zod schema

--- apps/backend/src/validation/auth.validate.ts
+++ apps/backend/src/validation/auth.validate.ts
@@ -1,28 +1,24 @@
 import { z } from "zod";
 
-const usernameValidation = z
+export const usernameValidation = z
   .string()
   .min(3, "username is too short")
-  .max(30, "username too long ");
+  .max(30, "username is too ")
+  .trim();
 
-const emailValidation = z.email("INvalid email");
+export const emailValidation = z.email("invalid email");
+export const passwordValidation = z
+  .string()
+  .min(4, "password is too short")
+  .regex(/[0-9]/, "password must contain atleast one number");
 
-const passwordValidation = z.string().min(5, `the password is too short `);
-
-export const signUpSchema = z.object({
+export const signupSchema = z.object({
   username: usernameValidation,
   email: emailValidation,
   password: passwordValidation,
 });
 
-export const signInSchema = z.object({
+export const signinSchema = z.object({
   email: emailValidation,
   password: passwordValidation,
 });
-// type SignUpInput = z.infer<typeof signUpSchema>;
-// type SignInInput = z.infer<typeof signInSchema>;
-
-// export type AuthInputs = {
-//   SignUpInput: z.infer<typeof signUpSchema>;
-//   SignInInput: z.infer<typeof signInSchema>;
-// };
